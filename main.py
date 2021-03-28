c4b13b0822c2472fe04003f9e07992b5eb1f9994
Firiza Stefania <stefaniafiriza@gmail.com>
2021-03-28 21:24:53 +0300

    Add adc for moisture and update for android app

--- main.py
+++ main.py
@@ -4,38 +4,63 @@ from gpiozero import DigitalInputDevice
 import RPi.GPIO as GPIO
 import adafruit_dht
 from board import *
+import busio
+import board
+import adafruit_ads1x15.ads1115 as ADS
+from adafruit_ads1x15.analog_in import AnalogIn
 import time
 
 GPIO.setup(26,GPIO.OUT)
 SENSOR_PIN = D22
 
 dht11 = adafruit_dht.DHT11(SENSOR_PIN, use_pulseio=False)
+i2c = busio.I2C(board.SCL,board.SDA)
+ads = ADS.ADS1115(i2c)
 
 app = Flask(__name__)
 api = Api(app)
 
 class DataBase(Resource):
     def get(self,id = 0):
+        print('"sensor": [\n{')
         if id == 0:
             return "Request invalid", 404
         if id == 1:
-            d0_input = DigitalInputDevice(17)
-            if not d0_input.value:
-                return 'threshold reached -> s1', 200
-            else:
-                return 'put water -> s1', 200
+            chan1 = AnalogIn(ads, ADS.P0)
+            dictionary = {
+                "sensors": {
+                    "sensor": "moisture 1",
+                    "value": chan1.voltage
+                    }
+                }
+            return dictionary, 200
         if id == 2:
-            d1_input = DigitalInputDevice(27)
-            if not d1_input.value:
-                return 'threshold reached -> s2', 200
-            else:
-                return 'put water -> s2', 200
+            chan2 = AnalogIn(ads, ADS.P1 )
+            dictionary = {
+                "sensors": {
+                    "sensor": "moisture 2",
+                    "value": chan2.voltage
+                    }
+                }
+            return dictionary, 200
         if id == 3:
             humidity = dht11.humidity
-            return humidity, 200
+            dictionary = {
+                "sensors": {
+                     "sensor": "humidity - aer",
+                     "value": humidity
+                    }
+                }
+            return dictionary, 200
         if id == 4:
             temperature = dht11.temperature
-            return temperature, 200
+            dictionary = {
+                "sensors": {
+                    "sensor": "temperature - aer",
+                    "value": temperature
+                    }
+                }
+            return dictionary, 200
         if id == 5:
             GPIO.output(26,True)
             time.sleep(5)
@@ -44,4 +69,4 @@ class DataBase(Resource):
         return "Not found", 404
 api.add_resource(DataBase,"/api","/api/<int:id>")
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug = True,host='0.0.0.0')
