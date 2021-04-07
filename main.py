749611aa6dde907eab8db11952c8940013d222a8
Firiza Stefania <stefaniafiriza@gmail.com>
2021-04-07 19:58:42 +0300

    Update detect weeds and update moisture sensor

--- main.py
+++ main.py
@@ -32,19 +32,13 @@ class DataBase(Resource):
             return "Request invalid", 404
         if id == 1:
             chan1 = AnalogIn(ads, ADS.P0)
+            chan2 = AnalogIn(ads, ADS.P1)
+            value = (chan1.voltage + chan2.voltage) / 2
+            value = '{0:.2f}'.format(value)
             dictionary = {
                 "sensors": {
-                    "sensor": "moisture 1",
-                    "value": chan1.voltage
-                    }
-                }
-            return dictionary, 200
-        if id == 2:
-            chan2 = AnalogIn(ads, ADS.P1 )
-            dictionary = {
-                "sensors": {
-                    "sensor": "moisture 2",
-                    "value": chan2.voltage
+                    "sensor": "moisture",
+                    "value": value
                     }
                 }
             return dictionary, 200
@@ -72,42 +66,48 @@ class DataBase(Resource):
             GPIO.output(26,False)
             return "the plants was watered"
         if id == 6:
-            img1 = cv2.imread("img1.jpeg")
-            img1=cv2.resize(img1,(480,600))
-            
-            hsv=cv2.cvtColor(img1,cv2.COLOR_BGR2HSV)
-            
-            l_b = np.array([22,28,37])
-            u_b = np.array([101,248,148])
-            
-            mask = cv2.inRange(hsv,l_b,u_b)
-            res = cv2.bitwise_and(img1,img1,mask=mask)
-            
-            median = cv2.medianBlur(mask,5)
-            cnts = cv2.findContours(median.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-            cnts = imutils.grab_contours(cnts)
-            green_area = 0
-            for c in cnts:
-                M = cv2.moments(c)
-                if M["m00"] > 0:
-                    cX = int((M["m10"]/M["m00"]))
-                    cY = int((M["m01"]/M["m00"]))
-                    peri = cv2.arcLength(c, True)
-                    approx = cv2.approxPolyDP(c, 0.04 * peri, True)
-                    cv2.drawContours(img1, [c], -1, (0, 255, 0), 2)
-                    area = cv2.contourArea(c)
-                    green_area = green_area + area
-            full_area = img1.shape[0]*img1.shape[1]/2
-            procent = (green_area * 100) / full_area
-            procent = '{0:.2f}'.format(procent)
-            dictionary = {
-                "sensors": {
-                    "sensor": "Detect weeds",
-                    "value": procent
+            cam = cv2.VideoCapture(0)
+            ok,img = cam.read()
+            if ok:
+                print("ok")
+                img=cv2.resize(img,(480,600))
+                hsv=cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+                
+                l_b = np.array([22,28,37])
+                u_b = np.array([101,248,148])
+                
+                mask = cv2.inRange(hsv,l_b,u_b)
+                res = cv2.bitwise_and(img,img,mask=mask)
+                
+                median = cv2.medianBlur(mask,5)
+                cnts = cv2.findContours(median.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+                cnts = imutils.grab_contours(cnts)
+                green_area = 0
+                for c in cnts:
+                    M = cv2.moments(c)
+                    if M["m00"] > 0:
+                        cX = int((M["m10"]/M["m00"]))
+                        cY = int((M["m01"]/M["m00"]))
+                        peri = cv2.arcLength(c, True)
+                        approx = cv2.approxPolyDP(c, 0.04 * peri, True)
+                        cv2.drawContours(img, [c], -1, (0, 255, 0), 2)
+                        area = cv2.contourArea(c)
+                        green_area = green_area + area
+                full_area = img.shape[0]*img.shape[1]/2
+                procent = (green_area * 100) / full_area
+                procent = '{0:.2f}'.format(procent)
+                cv2.waitKey(0)
+                dictionary = {
+                    "sensors": {
+                        "sensor": "Detect weeds",
+                        "value": procent
+                        }
                     }
-                }
-            return dictionary, 200
+                cam.release()
+                cv2.imwrite("result.jpg",img)
+                return dictionary, 200
         return "Not found", 404
 api.add_resource(DataBase,"/api","/api/<int:id>")
 if __name__ == '__main__':
     app.run(debug = True,host='0.0.0.0')
+ 
